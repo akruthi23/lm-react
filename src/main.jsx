ac4cd8f00229165a4598b701786822f321589cce
akruthi23 <spatel@loyaltymethods.com>
2025-08-29 15:53:36 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Comp1 from './components'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Comp1 />
   </StrictMode>,
 )
