9da9f3ac8bd5437d059c7018e628726e082916cb
akruthi23 <spatel@loyaltymethods.com>
2025-08-29 16:24:42 +0530

    xcvb

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Comp1 from './components'
+// import App from './App.jsx'/
+
+import Comp1 from './components/Comp1'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
